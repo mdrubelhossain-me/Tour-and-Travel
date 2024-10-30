6219065b4b6aed49e0c2e7a0a5a3ddf4af33f45c
mdrubelhossain-me <mdrubelhossain.cse@gmail.com>
2024-10-30 21:43:06 +0600

    project created- first commit

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css'
+import './HomeResponsive.css'
+
+const Home = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Home;
